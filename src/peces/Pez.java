15506fbb9a641981636110be238c9993711fbb4a
AlexVare <alex_varela_13@hotmail.com>
2023-10-03 19:20:36 +0200

    Carpeta peces

---
+++ src/peces/Pez.java
@@ -0,0 +1,18 @@
+package peces;
+public class Pez {
+    
+    public int edad=0;
+    public final String NOMBRECOMUN;
+    public final String NOMBRECIENT;
+    public final boolean sexo;
+    public boolean fertil;
+    public boolean vivo;
+
+    /**
+     * 
+     * 
+     */
+    public static void showStatus(){
+        
+    }
+}
