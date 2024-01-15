ca8ec6c0186ec33986c37dc22028dd5fab36b33f
AlexVare <alex_varela_13@hotmail.com>
2024-01-15 20:42:33 +0100

    granja vegetal

--- src/granjas/GranjaAnimal.java
+++ src/granjas/GranjaAnimal.java
@@ -16,4 +16,5 @@ public class GranjaAnimal {
         return instance;
     }
 
+    
 }
--- src/granjas/GranjaVegetal.java
+++ src/granjas/GranjaVegetal.java
@@ -2,5 +2,19 @@ package granjas;
 
 public class GranjaVegetal {
     
+    private static GranjaVegetal instance=null;
+    private int ciclo=0;
+
+    private GranjaVegetal(){
+        this.ciclo=3;
+    }
+
+    public static GranjaVegetal getInstance(){
+        if(instance==null){
+            instance= new GranjaVegetal();
+        }
+        return instance;
+    }
+
     
 }
